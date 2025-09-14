f2d2eabe1ec6f4274ff32a14eefcbfdb87cf7b04
Jakkapat Paijit <jakkapat.pai@ascendcorp.com>
2025-09-14 18:41:53 +0700

    🚑️ Fix projects page overflow

--- src/features/projects/Projects.tsx
+++ src/features/projects/Projects.tsx
@@ -12,7 +12,7 @@ interface Props {
 function Projects({ title, projects }: Props) {
     let navigate = useNavigate();
     return (
-        <div className="w-screen px-[108px] ">
+        <div className="px-[108px] ">
             <h2 className="text-h2 text-secondary font-bold pb-[48px]">{title}</h2>
             <div className="grid grid-cols-2 gap-[48px] pb-[48px]">
                 {projects.map((proj) => (
